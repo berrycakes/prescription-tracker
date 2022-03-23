2fc1581cb8e9137f19e81c65bafcfb896a8d85c8
Jianne Gabriel <jiannegabriel@gmail.com>
2022-03-23 14:11:34 +0800

    Fixed views to show doctor name and details instead of id

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,11 @@
 class Doctor < ApplicationRecord
     has_many :prescriptions
+
+    def descriptive_name
+        "Dr. #{self.first_name.titleize} #{self.last_name.titleize}, #{self.specialization}"
+    end
+
+    def full_name
+        "Dr. #{self.first_name.titleize} #{self.last_name.titleize}"
+    end
 end
